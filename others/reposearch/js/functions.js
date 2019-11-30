a9a549b5ddfe6d249adfd875a43de6269d1c179c
ivandax <ivandax_89@hotmail.com>
2019-11-30 21:14:45 +0100

    repo search initial connection working, styling ok

--- others/reposearch/js/functions.js
+++ others/reposearch/js/functions.js
@@ -1,52 +1,31 @@
-console.log("marfeel");
-
 //helper functions
 
 const get = (id) => {
     return document.getElementById(id);
 }
 
-//connect to github api
+//function to connect to github API.
 
-const connect = () => {
-
+const connect = (username) => {
     const request = new XMLHttpRequest();
+    request.open('GET', `https://api.github.com/users/${username}`, true);
 
-    
-}
-
-// var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-// request.onload = function() {
-//     var data = JSON.parse(this.response)
-
-//     if (request.status >= 200 && request.status < 400) {
-//       data.forEach(movie => {
-//         console.log(movie.title)
-//       })
-//     } else {
-//       console.log('error')
-//     }
-// }
-
-// request.send()
-
-var request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.github.com/users/ivandax', true)
-
-request.onload = function() {
-    var data = JSON.parse(this.response)
-
-    if (request.status >= 200 && request.status < 400) {
-        console.log(data);
-    } else {
-        console.log('error')
+    request.onload = function() {
+        const data = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data);
+        } else {
+            console.log('error')
+        }
     }
+    request.send();
 }
 
-request.send()
+const form = get("form").addEventListener('submit', (event) => {
+    event.preventDefault();
+    user = get("input").value;
+    console.log(user)
+    connect(user);
+})
+
+
